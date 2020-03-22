e50c2dd5ea6d41c766cf8aaee381385c4c092ff9
Evan <530788697@qq.com>
2020-03-22 20:10:38 +0800

    MMP

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng.manage_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author evanYang
+ * @version 1.0
+ * @date 2020/03/22 18:16
+ */
+@SpringBootApplication
+public class ManageCourseApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCourseApplication.class,args);
+    }
+}
