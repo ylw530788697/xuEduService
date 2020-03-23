c010e36d321129d06d6b8f80ab679876c0f222fe
Evan <530788697@qq.com>
2020-03-23 22:18:48 +0800

    2.2.5

--- xc-govern-center/src/main/java/com/xuecheng/govern/GovernCenterApplication.java
+++ xc-govern-center/src/main/java/com/xuecheng/govern/GovernCenterApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class GovernCenterApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GovernCenterApplication.class,args);
     }
