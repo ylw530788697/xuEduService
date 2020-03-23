ec6409181593be0be1ded6fc5fe30307cde8367c
Evan <530788697@qq.com>
2020-03-23 15:43:26 +0800

    init

---
+++ xc-govern-center/src/main/java/com/xuecheng/govern/GovernCenterApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng.govern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author evanYang
+ * @version 1.0
+ * @date 2020/03/23 15:24
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class GovernCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GovernCenterApplication.class,args);
+    }
+}
