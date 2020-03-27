ca85c9ef09b2e867e5db36e50f7728fbc06ba56e
ylw530788697 <530788697@qq.com>
2020-03-27 18:16:45 +0800

    登录

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
@@ -2,9 +2,9 @@ package com.xuecheng.manage_course.client;
 
 import com.xuecheng.framework.domain.cms.request.QueryPageReq;
 import com.xuecheng.framework.model.response.ResponseData;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,8 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "xc-service-manage-cms") //指定远程调用的服务名
 public interface CmsPageClient {
     @GetMapping("/list/{page}/{size}")
-    public ResponseData<?> findList(@RequestParam("page") Integer page, @RequestParam("size")Integer size, @RequestBody QueryPageReq req);
+    public ResponseData<?> findList(@RequestParam("page") Integer page, @RequestParam("size") Integer size, @RequestBody QueryPageReq req);
+
     @GetMapping("/demo")
-    public ResponseData<?> findDemo(@RequestParam("name") String name) ;
+    public ResponseData<?> findDemo(@RequestParam("name") String name);
 
 }
